a1365d68941d5751498dab9f505a5358da02a999
ajhanson22227 <ajhanson22227@gmail.com>
2021-01-14 15:49:26 -0600

    Import personal summary component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Welcome from "./components/Welcome/WelcomePage";
+import Personal from "./components/Summary/Personal/Personal";
 
 const App = () => {
   return (
     <div className="main-container">
       <Welcome />
+      <Personal />
     </div>
   );
 };
