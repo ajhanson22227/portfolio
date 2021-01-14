452c5305daf321c5fbd35fa03cfb9ef4df524204
ajhanson22227 <ajhanson22227@gmail.com>
2021-01-14 16:46:19 -0600

    Initialize Summary Componenet Will contain personal and skills components

---
+++ src/components/Summary/Summary.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Personal from "./Personal/Personal";
+import "./Summary.css";
+
+const Summary = () => {
+  return (
+    <div className="summary-container">
+      <Personal />
+    </div>
+  );
+};
+
+export default Summary;
