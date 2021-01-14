7976be9c2cf42b2c1502c6e507c2e978598ed5e0
ajhanson22227 <ajhanson22227@gmail.com>
2021-01-14 14:35:37 -0600

    Initialize welcome page component

---
+++ src/components/Welcome/WelcomePage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Welcome.css";
+
+const WelcomePage = () => {
+  return (
+    <div className="welcome-container">
+      <div>
+        HELLO I AM <span className="bold-text">depressed</span>
+      </div>
+      <div>
+        I AM A <span className="bold-text">FULL STACK DEVELOPER</span>
+      </div>
+    </div>
+  );
+};
+
+export default WelcomePage;
