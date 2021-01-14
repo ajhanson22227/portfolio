7c1e0f3b31aa296892b524f1921b0a0ebdb52b16
ajhanson22227 <ajhanson22227@gmail.com>
2021-01-14 16:45:25 -0600

    Add Personal component with Styling

--- src/components/Summary/Personal/Personal.js
+++ src/components/Summary/Personal/Personal.js
@@ -1,7 +1,22 @@
 import React from "react";
+import "./Personal.css";
+import facepic from "../../../assets/facepic.png";
 
 const Personal = () => {
-  return <div>Hello Porld</div>;
+  return (
+    <div className="personal-container df-row">
+      <div className="personal-picture">
+        <img src={facepic} alt="adam, the site creator" />
+      </div>
+      <div className="about-content">
+        <div className="about-title">About yo</div>
+        <div className="about-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore.
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Personal;
