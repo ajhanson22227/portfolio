3f965db351e1e2189a4ece4c770979444f515a0f
ajhanson22227 <ajhanson22227@gmail.com>
2021-01-14 16:50:49 -0600

    Initialize skills component

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Personal from "./Personal/Personal";
+import Skills from "./Skills/Skills";
 import "./Summary.css";
 
 const Summary = () => {
   return (
     <div className="summary-container">
       <Personal />
+      <Skills />
     </div>
   );
 };
