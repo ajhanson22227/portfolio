577c3451d2b9f841ed566bfdabfa9f28d83eb04f
ajhanson22227 <ajhanson22227@gmail.com>
2021-01-15 14:45:37 -0600

    Import Project component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Welcome from "./components/Welcome/WelcomePage";
 import Summary from "./components/Summary/Summary";
+import Project from "./components/Project/Project";
 
 const App = () => {
   return (
     <div className="main-container">
       <Welcome />
       <Summary />
+      <Project />
     </div>
   );
 };
