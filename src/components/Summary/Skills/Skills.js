aba97a376f6d9629011cda922edec298aea6cb33
ajhanson22227 <ajhanson22227@gmail.com>
2021-01-15 10:14:47 -0600

    Add divs for each skill I have

--- src/components/Summary/Skills/Skills.js
+++ src/components/Summary/Skills/Skills.js
@@ -1,10 +1,21 @@
 import React from "react";
+import "./Skills.css";
 
 const Skills = () => {
   return (
-    <div className="skills-container">
-      <div></div>
-      <div></div>
+    <div className="skills-outside-container">
+      <div className="skills-inside-container">
+        <div className="skills-title">Languages, Frameworks & Libraries</div>
+        <div className="skills-container">
+          <p className="skill b-right">HTML</p>
+          <p className="skill b-right">CSS</p>
+          <p className="skill b-right">JS</p>
+          <p className="skill b-right">React</p>
+          <p className="skill b-right">NodeJS + Express</p>
+          <p className="skill b-right">MongoDB + Mongoose</p>
+          <p className="skill">C++</p>
+        </div>
+      </div>
     </div>
   );
 };
