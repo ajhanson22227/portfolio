a2f6411d3934bf74682186e472141400799d0073
ajhanson22227 <ajhanson22227@gmail.com>
2021-01-19 11:14:30 -0600

    Import Contact component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Welcome from "./components/Welcome/WelcomePage";
 import Summary from "./components/Summary/Summary";
 import Project from "./components/Project/Project";
+import Contact from "./components/Contact/Contact";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Welcome />
       <Summary />
       <Project />
+      <Contact />
     </div>
   );
 };
