9f182e39d1c2d6958bc33849def38792dca731b7
ajhanson22227 <ajhanson22227@gmail.com>
2021-01-19 17:00:26 -0600

    Add wrapper to arrow Clicking bouncing arrow was too dificult

--- src/components/Welcome/WelcomePage.js
+++ src/components/Welcome/WelcomePage.js
@@ -11,9 +11,11 @@ const WelcomePage = (props) => {
         I AM A <span className="bold-text">FULL STACK DEVELOPER</span>
       </div>
       <div
+        className="welcome-content arrow-container"
         onClick={() => props.ScrollTo(props.aboutRef)}
-        className="welcome-content arrow arrow-down bounce-2"
-      ></div>
+      >
+        <div className="welcome-content arrow arrow-down bounce-2"></div>
+      </div>
     </div>
   );
 };
