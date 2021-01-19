d2d96f132b9b2334d6b18efcd82ae98dcdfe2b5b
ajhanson22227 <ajhanson22227@gmail.com>
2021-01-19 16:03:10 -0600

    Make scroll to function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 import Welcome from "./components/Welcome/WelcomePage";
 import Summary from "./components/Summary/Summary";
 import Project from "./components/Project/Project";
 import Contact from "./components/Contact/Contact";
 
 const App = () => {
+  const aboutRef = useRef();
+  // const projectRef = useRef();
+  // const contactRef = useRef();
+
+  const ScrollTo = (ref) => {
+    ref.current.scrollIntoView({ behavior: "smooth", block: "start" });
+  };
+
   return (
     <div className="main-container">
-      <Welcome />
-      <Summary />
+      <Welcome aboutRef={aboutRef} ScrollTo={ScrollTo} />
+      <Summary aboutRef={aboutRef} ScrollTo={ScrollTo} />
       <Project />
       <Contact />
     </div>
