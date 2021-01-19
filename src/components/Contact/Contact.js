41f8678592c7bdb2f97e5815b62a27e18f8b40cf
ajhanson22227 <ajhanson22227@gmail.com>
2021-01-19 13:39:53 -0600

    Create contact component and add information

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Contact.css";
+
+const Contact = () => {
+  return (
+    <div className="contact-container">
+      <div className="contact-content-container">
+        <div className="contact-topline"></div>
+        <div className="contact-title">CONTACT</div>
+        <div className="contact-desc">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore.
+        </div>
+        <div className="contact-email">AJhanson4535345@gmail.com</div>
+        <div className="contact-also">You can also find me here:</div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
