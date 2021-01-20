e339ec739bb213ba7c4180302d1df5ea33a83d0a
ajhanson22227 <ajhanson22227@gmail.com>
2021-01-20 11:33:45 -0600

    Increase height when navbar appears

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      window.pageYOffset >= window.innerHeight - 20
+      window.pageYOffset >= window.innerHeight - 50
         ? setIsScrolled(true)
         : setIsScrolled(false);
     };
